6e081fbd82c3d044d352df6b0ec9c69e2732d9d9
Muskaantawry <65067255+Muskaantawry@users.noreply.github.com>
2020-06-12 23:11:03 +0530

    solution for doubt 3

---
+++ doubt_sol.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin >> a >> b >> c >> d >> e;
+    cout <<"\n you entered:\n";
+    cout <<a <<"\n"<<b <<"\n"<<c <<"\n" <<d <<"\n" <<e;
+    return 0;
+}
