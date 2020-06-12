b90ef5d24e2c2a6d55a412565691ebb3f5a99365
Muskaantawry <65067255+Muskaantawry@users.noreply.github.com>
2020-06-12 11:41:09 +0530

    Update doubt_statement.cpp

--- Doubt Contest/3/doubt_statement.cpp
+++ Doubt Contest/3/doubt_statement.cpp
@@ -1,7 +1,7 @@
 //The code should take all the values from int, long, char, float and doubt but after I enter char value, it stops taking other values and gives the output.This is my problem sir
 
 #include <iostream>
-#include <cstdio>
+
 using namespace std;
 
 int main() {
@@ -10,9 +10,8 @@ int main() {
     char c;
     float d;
     double e;
-    printf("Enter the inputs:");
-	scanf("%d%li%c%f%lf",&a,&b,&c,&d,&e);
-	printf("\nOutput:\n");
-	printf("%d\n%li\n%c\n%f\n%lf\n",a,b,c,d,e);
+    cin >> a >> b >> c >> d >> e;
+    cout <<"\n you entered:\n";
+    cout <<a <<"\n"<<b <<"\n"<<c <<"\n" <<d <<"\n" <<e;
     return 0;
 }
